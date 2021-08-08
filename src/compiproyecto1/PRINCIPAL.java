bdd74134fb75439d5d20e41f7bf120f42eebfcb4
Rodrigo Eduardo Carcuz Ortega <53447333+carcuz789@users.noreply.github.com>
2021-08-08 11:36:06 -0600

    DD

--- src/compiproyecto1/PRINCIPAL.java
+++ src/compiproyecto1/PRINCIPAL.java
@@ -33,11 +33,11 @@ public class PRINCIPAL extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 563, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 383, Short.MAX_VALUE)
         );
 
         pack();
